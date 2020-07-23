2ea7c194f40c6fb47ac6bda1de7203833021e091
Eric McIntyre <emcintyre@cyberscout.com>
2020-07-23 14:37:09 -0700

    Shell classes for Java 7 features

---
+++ src/main/java/com/notaneye/learn/java7/NumericLiterals.java
@@ -0,0 +1,6 @@
+package com.notaneye.learn.java7;
+
+
+public class NumericLiterals {
+
+}
---
+++ src/main/java/com/notaneye/learn/java7/StringsInSwitch.java
@@ -0,0 +1,9 @@
+package com.notaneye.learn.java7;
+
+
+public class StringsInSwitch {
+
+    public static void main(String[] args) {
+
+    }
+}
