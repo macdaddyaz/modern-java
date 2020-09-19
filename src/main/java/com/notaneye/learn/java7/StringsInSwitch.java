a49d42893c62e70f55bcaf4e1fcee4494572495f
Eric McIntyre <emcintyre@cyberscout.com>
2020-09-18 17:29:31 -0700

    Implemented strings-in-switch example

--- src/main/java/com/notaneye/learn/java7/StringsInSwitch.java
+++ src/main/java/com/notaneye/learn/java7/StringsInSwitch.java
@@ -5,5 +5,21 @@ public class StringsInSwitch {
 
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.err.println("😡 I've come here for a good argument");
+            System.exit(1);
+        }
+        String arg = args[0];
+        //noinspection EnhancedSwitchMigration
+        switch (arg) {
+        case "parrot":
+            System.out.println("🦜 He's probably pining for the fjords");
+            break;
+        case "swallow":
+            System.out.println("🥥 A five ounce bird cannot carry a one pound coconut!");
+            break;
+        default:
+            System.out.println("🤷‍ You've stumped me");
+        }
     }
 }
