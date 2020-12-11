e54469f0d16ff85d876bdea644f9922bbfc6ebfb
Eric McIntyre <emcintyre@cyberscout.com>
2020-12-11 16:00:16 -0700

    Java 9+: Private interface methods, type inference
    Add examples for private interface methods and local variable type
    inference.

--- java9plus/src/main/java/com/notaneye/learn/java10/VariableTypeInference.java
+++ java9plus/src/main/java/com/notaneye/learn/java10/VariableTypeInference.java
@@ -1,6 +1,37 @@
 package com.notaneye.learn.java10;
 
 
+import java.util.Arrays;
+
+
+// The `var` keyword was introduced as a shorthand variable declaration. The
+// variable it declares is still fully typed, but the type is inferred from the
+// right-hand side of the statement.
+//
+// Type inference only works in the following places:
+// - local variables with initializers
+// - indexes in the enhanced for-loop
+// - locals declared in a traditional for-loop
 public class VariableTypeInference {
 
+    @SuppressWarnings({ "CommentedOutCode", "ForLoopReplaceableByForEach" })
+    public static void main(String[] args) {
+
+        var someNumbers = new int[] { 0, 2, 4, 6, 8 };
+        // `someNumbers` is known to be of type `int[]`
+        for (var i = 0; i < someNumbers.length; i++) {
+            System.out.println(someNumbers[i]);
+        }
+        // The following line will not compile
+        // someNumbers = new Object();
+        var list = Arrays.asList("X", "Y", "Z");
+        // `list` is known to be of type `List<String>`
+        list.add(0, "A");
+        for (var s : list) {
+            System.out.println(s);
+        }
+        // The following lines will not compile
+        // list.add(LocalDate.now());
+        // list = new ArrayList<Object>();
+    }
 }
--- java9plus/src/main/java/com/notaneye/learn/java9/PrivateInterfaceMethods.java
+++ java9plus/src/main/java/com/notaneye/learn/java9/PrivateInterfaceMethods.java
@@ -1,6 +1,39 @@
 package com.notaneye.learn.java9;
 
 
+// In addition to default implementations of interface methods, interfaces can
+// define private instance methods. These methods can only operate on their
+// parameters and invoke other methods of the interface.
+@SuppressWarnings("unused")
 public class PrivateInterfaceMethods {
 
+    private interface DoorKnob {
+
+        void lock();
+
+        void unlock();
+
+        void unlatch();
+
+        default void turn() {
+
+            if (!isLocked()) {
+                unlatch();
+            }
+            else {
+                throwLockedException();
+            }
+        }
+
+        boolean isLocked();
+
+        boolean isLatched();
+
+        // `private` really means private. Neither implementors nor
+        // sub-interfaces can access this method.
+        private void throwLockedException() {
+
+            throw new IllegalStateException("Door knob is locked");
+        }
+    }
 }
