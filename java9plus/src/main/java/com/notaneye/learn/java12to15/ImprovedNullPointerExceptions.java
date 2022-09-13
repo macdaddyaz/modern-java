0bbad465c51b8f7a1f6377214e10fb7c0623989e
Eric McIntyre <eric.mcintyre@hpe.com>
2022-09-13 15:50:47 -0700

    Add example for improved NPEs

--- java9plus/src/main/java/com/notaneye/learn/java12to15/ImprovedNullPointerExceptions.java
+++ java9plus/src/main/java/com/notaneye/learn/java12to15/ImprovedNullPointerExceptions.java
@@ -3,4 +3,28 @@ package com.notaneye.learn.java12to15;
 
 public class ImprovedNullPointerExceptions {
 
+    public static void main(String[] args) {
+
+        methodA();
+    }
+
+    private static void methodA() {
+
+        methodB();
+    }
+
+    private static void methodB() {
+
+        methodToMyMadness();
+    }
+
+    @SuppressWarnings({"java:S2201", "ResultOfMethodCallIgnored"})
+    private static void methodToMyMadness() {
+        var nested = new SomethingNested(null);
+        nested.value().length();
+    }
+
+    private record SomethingNested(String value) {
+
+    }
 }
